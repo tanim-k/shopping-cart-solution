b33517f900e8c103ced410bc1026dac2238c8141
tanim-k <tanimkhan877@gmail.com>
2022-02-16 09:39:02 +0600

    add deduct cart total

--- js/without-fn.js
+++ js/without-fn.js
@@ -1,1 +1,19 @@
-console.log('I am in a separate file')
+console.log('I am in a separate file. here, no function is used to solve problems')
+//🎡 basic problem solving learning:
+
+// 1: cass plus system:
+document.getElementById('case-plus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumberText = caseInput.value;
+    const caseNumber = parseInt(caseNumberText);
+    caseInput.value = caseNumber+1;
+    console.log(caseInput.value); 
+ })
+ // 2: case deduct system 
+ document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumberText = caseInput.value;
+    const caseNumber = parseInt(caseNumberText);
+    caseInput.value = caseNumber-1;
+    console.log(caseInput.value);
+ })
